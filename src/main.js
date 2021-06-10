166ae3f9c21d088a4ac1c48d020e9d3891d29727
Cruii <cruii811@gmail.com>
2021-06-10 22:35:45 +0800

    :lipstick: style(ui): 修改v-switcher样式
    将开关的label放在前方

--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@ import App from './App.vue'
 import router from './router'
 import vuetify from './plugins/vuetify'
 import axios from 'axios'
+import './assets/css/global.less'
+axios.defaults.withCredentials = true
 Vue.prototype.$http = axios
 const host = 'https://api.cruii.io/bilibili/'
 axios.defaults.baseURL = host
