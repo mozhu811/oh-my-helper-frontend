bfd5aea32b65d19efdb0adb22fcaaa831a17fe5a
Cruii <cruii811@gmail.com>
2021-06-10 22:38:26 +0800

    :sparkles: feat(common): 添加获取cookie方法

--- src/main.js
+++ src/main.js
@@ -11,6 +11,15 @@ axios.defaults.baseURL = host
 
 Vue.config.productionTip = false
 
+Vue.prototype.getCookie = function (cname) {
+  const name = cname + '='
+  const ca = document.cookie.split(';')
+  for (let i = 0; i < ca.length; i++) {
+    const c = ca[i].trim()
+    if (c.indexOf(name) === 0) return c.substring(name.length, c.length)
+  }
+  return null
+}
 new Vue({
   router,
   vuetify,
