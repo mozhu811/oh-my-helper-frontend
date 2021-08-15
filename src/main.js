8531608431cabd786917bfe8218001b9401d12b9
Cruii <cruii811@gmail.com>
2021-08-15 11:39:13 +0800

    :fire: refactor(cookie操作): 删除废弃代码

--- src/main.js
+++ src/main.js
@@ -10,16 +10,6 @@ const host = 'https://api.cruii.io/bilibili/'
 axios.defaults.baseURL = host
 
 Vue.config.productionTip = false
-
-Vue.prototype.getCookie = function (cname) {
-  const name = cname + '='
-  const ca = document.cookie.split(';')
-  for (let i = 0; i < ca.length; i++) {
-    const c = ca[i].trim()
-    if (c.indexOf(name) === 0) return c.substring(name.length, c.length)
-  }
-  return null
-}
 new Vue({
   router,
   vuetify,
