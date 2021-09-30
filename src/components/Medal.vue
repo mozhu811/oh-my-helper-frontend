78a0f3dbcaa753f5233ea6fb9f1e18c5647631b3
Cruii <cruii811@gmail.com>
2021-09-30 11:06:50 +0800

    :sparkles: feat(card): 新增勋章墙

---
+++ src/components/Medal.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <div class="medal m-medal__fans-medal-item" :style="'border-color:' + colorBorderHex">
+      <div class="m-medal__fans-medal-label"
+           :style="'background-image: linear-gradient(45deg,' + colorStartHex + ',' + colorEndHex +');'">
+        <span class="m-medal__fans-medal-content">{{ name }}</span>
+      </div>
+      <div class="m-medal__fans-medal-level" :style="'color:' + colorBorderHex">
+        {{ level }}
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    colorStart: Number,
+    colorEnd: Number,
+    colorBorder: Number,
+    level: Number,
+    name: String
+  },
+  computed: {
+    colorStartHex: function () {
+      return '#' + this.colorStart.toString(16)
+    },
+    colorEndHex: function () {
+      return '#' + this.colorEnd.toString(16)
+    },
+    colorBorderHex: function () {
+      return '#' + this.colorBorder.toString(16)
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
