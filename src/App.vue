23c44e5b2438786810a78efef6a6d2fdd508a500
Cruii <cruii811@gmail.com>
2021-12-27 10:59:47 +0800

    :fire: refactor(首页): 删除悬浮按钮

--- src/App.vue
+++ src/App.vue
@@ -67,20 +67,6 @@
     </v-app-bar>
 
     <v-main>
-      <!-- 底部按钮 -->
-      <v-btn fab dark large color="pink" fixed right bottom style="bottom: 155px">
-        <v-icon dark>mdi-heart</v-icon>
-      </v-btn>
-      <v-btn fab dark large color="pink" fixed right bottom style="bottom: 85px">
-        <v-icon dark>mdi-heart</v-icon>
-      </v-btn>
-      <v-btn fab dark large color="pink" fixed right bottom>
-        <v-icon dark>mdi-heart</v-icon>
-      </v-btn>
-      <!-- 中间按钮 -->
-
-      <!-- 顶部按钮 -->
-
       <router-view/>
     </v-main>
 
