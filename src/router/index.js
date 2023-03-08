ffe00359ce0404233d6cf9754451f0f63eb3bf68
cruii <cruii811@gmail.com>
2023-03-08 17:36:03 +0800

    :new: 新增路由

--- src/router/index.js
+++ src/router/index.js
@@ -1,30 +1,46 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Bilibili from '../views/Bilibili.vue'
-import Error from '@/views/Error.vue'
+import Error from '../views/Error.vue'
+import ComingSoon from '../views/ComingSoon.vue'
+import Login from '../views/Login.vue'
+import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
 const originalPush = VueRouter.prototype.push
 
-VueRouter.prototype.push = function push(location) {
+VueRouter.prototype.push = function push (location) {
   return originalPush.call(this, location).catch(err => err)
 }
 
 const routes = [
   {
+    path: '/login',
+    name: 'login',
+    component: Login
+  },
+  {
     path: '/',
     name: 'home',
-    redirect: 'bilibili'
-  },
-  {
-    path: '/bilibili',
-    name: 'bilibili',
-    component: Bilibili
-  },
-  {
-    path: '/error',
-    name: 'error',
-    component: Error
+    component: Home,
+    redirect: '/bilibili',
+    children: [
+      {
+        path: '/bilibili',
+        name: 'bilibili',
+        component: Bilibili
+      },
+      {
+        path: '/new',
+        name: 'new',
+        component: ComingSoon
+      },
+      {
+        path: '/error',
+        name: 'error',
+        component: Error
+      }
+    ]
   }
 ]
 
